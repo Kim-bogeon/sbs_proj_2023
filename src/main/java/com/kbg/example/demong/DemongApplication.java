51e48fdc65458bf6bfd70b8c9418ab1fd8698408
Kim-bogeon <posoii@naver.com>
2023-04-11 18:16:41 +0900

    첫 커밋

---
+++ src/main/java/com/kbg/example/demong/DemongApplication.java
@@ -0,0 +1,13 @@
+package com.kbg.example.demong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbg/example/demong/DemongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbg.example.demong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d83851d259701ab50b97b191c564565a297dbec6
Kim-bogeon <posoii@naver.com>
2023-04-13 16:28:31 +0900

    게시물 컨트롤러에 게시물 추가, 리스트 구현 기능 구현

--- src/main/java/com/kbg/example/demong/controller/UsrHomeController.java
+++ src/main/java/com/kbg/example/demong/controller/UsrHomeController.java
@@ -13,16 +13,10 @@ public class UsrHomeController {
 		count = -1;
 	}
 	
-	//get, do 는 show를 안 붙인다.
-	@RequestMapping("/usr/home/getCount")
-	@ResponseBody
-	public int getCount() {
-		return count;
-	}
 	
-	@RequestMapping("/usr/home/doSetCount")
+	@RequestMapping("/usr/home/main")
 	@ResponseBody
-	public String doSetCount(int count) {
+	public String showMain(int count) {
 		this.count = count;
 		return "count의 값이 " + this.count + "으로 초기화.";
 	}
